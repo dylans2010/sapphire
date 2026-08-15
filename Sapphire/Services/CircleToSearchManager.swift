bcbde2c977859c81848578a8bbe1b6a664d2affc
Dylan <galaxy.warship5r@icloud.com>
2026-08-15 13:43:56 -0700

    Fix circle to search notification name

--- Sapphire/Services/CircleToSearchManager.swift
+++ Sapphire/Services/CircleToSearchManager.swift
@@ -1,5 +1,9 @@
 import Foundation
 
+extension Notification.Name {
+    static let sapphireOpenCircleToSearch = Notification.Name("sapphireOpenCircleToSearch")
+}
+
 @MainActor
 final class CircleToSearchManager: ObservableObject {
     static let shared = CircleToSearchManager()
